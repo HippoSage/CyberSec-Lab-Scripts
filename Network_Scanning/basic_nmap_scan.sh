ec0e5f26117db423a1f18d082950e61f01061f24
HippoSage <thanos2208@outlook.com>
2025-05-09 23:10:59 -0500

    Update basic_nmap_scan.sh

--- Network_Scanning/basic_nmap_scan.sh
+++ Network_Scanning/basic_nmap_scan.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# -----------------------------------------------------------------------------
+# Script: Basic Nmap Scan
+# Author: By Gross
+# Description: Performs a basic TCP SYN scan with version detection using Nmap.
+# Certifications: ISC2 CC, SSCP (in progress)
+# Education: MS in Information Systems (Expected March 2026)
+# GitHub: github.com/sarcasticwitwizard
+# LinkedIn: linkedin.com/in/philliplgross
+# -----------------------------------------------------------------------------
+
 echo "Enter target IP:"
 read IP
 nmap -sS -sV -T4 $IP
