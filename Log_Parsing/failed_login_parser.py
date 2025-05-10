a82032a4771e46fa604217f0ae779f8f6b2cc1bd
HippoSage <thanos2208@outlook.com>
2025-05-09 23:11:22 -0500

    Update failed_login_parser.py

--- Log_Parsing/failed_login_parser.py
+++ Log_Parsing/failed_login_parser.py
@@ -1,4 +1,12 @@
-import re
+# -----------------------------------------------------------------------------
+# Script: Failed SSH Login Parser
+# Author: By Gross
+# Description: Parses auth.log for failed SSH login attempts.
+# Certifications: ISC2 CC, SSCP (in progress)
+# Education: MS in Information Systems (Expected March 2026)
+# GitHub: github.com/sarcasticwitwizard
+# LinkedIn: linkedin.com/in/philliplgross
+# -----------------------------------------------------------------------------
 
 with open("/var/log/auth.log", "r") as file:
     for line in file:
