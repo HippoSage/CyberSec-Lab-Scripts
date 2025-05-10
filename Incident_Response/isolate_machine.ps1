a7a81e29015f2d1267544bc27d6c56a0dfbdfbdf
HippoSage <thanos2208@outlook.com>
2025-05-09 23:12:51 -0500

    Update isolate_machine.ps1

--- Incident_Response/isolate_machine.ps1
+++ Incident_Response/isolate_machine.ps1
@@ -1,3 +1,12 @@
-# Blocks all inbound and outbound connections to isolate a Windows machine from the network
+# -----------------------------------------------------------------------------
+# Script: Host Isolation Script
+# Author: By Gross
+# Description: Blocks all inbound/outbound traffic to isolate a compromised host.
+# Certifications: ISC2 CC, SSCP (in progress)
+# Education: MS in Information Systems (Expected March 2026)
+# GitHub: github.com/sarcasticwitwizard
+# LinkedIn: linkedin.com/in/philliplgross
+# -----------------------------------------------------------------------------
+
 New-NetFirewallRule -DisplayName "IsolateHost-Out" -Direction Outbound -Action Block -Enabled True -Profile Any
 New-NetFirewallRule -DisplayName "IsolateHost-In" -Direction Inbound -Action Block -Enabled True -Profile Any
